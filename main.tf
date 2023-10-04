669b0bcafa94c815a89d60ad8c6ee8c3d5c1045a
sindhuja gutha <sindhugutha88@gmail.com>
2023-10-04 11:26:03 -0500

    the commits are done properly

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
-module "components" {
+module "vpc" {
   source          = "git::https://github.com/SindhuGutha09/tf-module-vpc.git"
+
   for_each        = var.vpc
   cidr = each.value["cidr"]
   subnets = each.value["subnets"]
+}
+
+output "vpc" {
+  value = module.vpc
 }
